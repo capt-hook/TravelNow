4083870680363a4ab1080358087fdb661f53fcee
Max.Huk <maksym.huk@symphony-solutions.eu>
2014-08-30 05:09:51 +0300

    Initial Commit

---
+++ TravelNow/TNAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TNAppDelegate.h
+//  TravelNow
+//
+//  Created by Maksym Huk on 8/30/14.
+//  Copyright (c) 2014 Maksym Huk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TNAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TravelNow/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TravelNow
+//
+//  Created by Maksym Huk on 8/30/14.
+//  Copyright (c) 2014 Maksym Huk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TNAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([TNAppDelegate class]));
+	}
+}
